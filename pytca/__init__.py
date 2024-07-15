d4155d9bbacec3ce9c450fc59709cfb08b5d0b67
Jialue Chen <jialuechen@outlook.com>
2024-07-15 18:51:59 -0400

    update version

--- pytca/__init__.py
+++ pytca/__init__.py
@@ -1,5 +1,4 @@
 __title__ = 'pytca'
-__version__ = '0.1.2'
-__author__ = 'Saeed Amen, Cuemacro'
+__version__ = '0.1.3'
+__author__ = 'Jialue Chen'
 __license__ = 'Apache 2'
-__copyright__ = 'Copyright 2021 by Cuemacro'
