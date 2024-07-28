80ed568acc8d5b87b0febd36d94822fe6389406c
Jialue Chen <jialuechen@outlook.com>
2024-07-27 22:20:54 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,6 @@ from setuptools import setup, Extension,find_packages
 from setuptools.command.build_ext import build_ext
 import pybind11
 
-ext_modules = [
-    Extension(
-        'pytca_cpp',
-        ['pytca/cpp/pybind_module.cpp'],
-        include_dirs=[pybind11.get_include()],
-        language='c++',
-    ),
-]
 
 setup(
     name='pytca',
@@ -22,7 +14,6 @@ setup(
     install_requires=[
         'pandas', 'matplotlib', 'plotly', 'bokeh', 'dash', 'flask', 'pybind11', 'numpy', 'geopandas', 'sklearn', 'textblob', 'requests', 'web3'
     ],
-    ext_modules=ext_modules,
     cmdclass={'build_ext': build_ext},
 )
 
